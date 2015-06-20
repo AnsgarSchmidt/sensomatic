7960dd42102e24c823d053eaa9a29120036ad541
Ansgar Schmidt <ansgar.schmidt@msasafety.com>
2015-06-21 01:13:10 +0200

    talking mqtt and redis version1

--- server/Persistor.py
+++ server/Persistor.py
@@ -3,76 +3,93 @@ import redis
 import threading
 import ConfigParser
 import paho.mqtt.client as mqtt
+import time
 
 class Persistor(threading.Thread):
 
-    def readConfig(self):
+    def _readConfig(self):
         update = False
 
-        if not os.path.isdir(self.homeDir):
+        if not os.path.isdir(self._homeDir):
             print "Creating homeDir"
-            os.makedirs(self.homeDir)
+            os.makedirs(self._homeDir)
 
-        if os.path.isfile(self.configFileName):
-            self.config.read(self.configFileName)
+        if os.path.isfile(self._configFileName):
+            self._config.read(self._configFileName)
         else:
             print "Config file not found"
             update = True
 
-        if not self.config.has_section('MQTT'):
+        if not self._config.has_section('MQTT'):
             print "Adding MQTT part"
             update = True
-            self.config.add_section("MQTT")
+            self._config.add_section("MQTT")
 
-        if not self.config.has_option("MQTT", "ServerAddress"):
+        if not self._config.has_option("MQTT", "ServerAddress"):
             print "No Server Address"
             update = True
-            self.config.set("MQTT", "ServerAddress", "<ServerAddress>")
+            self._config.set("MQTT", "ServerAddress", "<ServerAddress>")
 
-        if not self.config.has_option("MQTT", "ServerPort"):
+        if not self._config.has_option("MQTT", "ServerPort"):
             print "No Server Port"
             update = True
-            self.config.set("MQTT", "ServerPort", "<ServerPort>")
+            self._config.set("MQTT", "ServerPort", "1883")
 
-        if not self.config.has_section('REDIS'):
+        if not self._config.has_section('REDIS'):
             print "Adding Redis part"
             update = True
-            self.config.add_section("REDIS")
+            self._config.add_section("REDIS")
 
-        if not self.config.has_option("REDIS", "ServerAddress"):
+        if not self._config.has_option("REDIS", "ServerAddress"):
             print "No Server Address"
             update = True
-            self.config.set("REDIS", "ServerAddress", "<ServerAddress>")
+            self._config.set("REDIS", "ServerAddress", "<ServerAddress>")
 
-        if not self.config.has_option("REDIS", "ServerPort"):
+        if not self._config.has_option("REDIS", "ServerPort"):
             print "No Server Port"
             update = True
-            self.config.set("REDIS", "ServerPort", "<ServerPort>")
+            self._config.set("REDIS", "ServerPort", "6379")
 
         if update:
-            with open(self.configFileName, 'w') as f:
-                self.config.write(f)
+            with open(self._configFileName, 'w') as f:
+                self._config.write(f)
 
     def __init__(self):
         threading.Thread.__init__(self)
         self.setDaemon(True)
 
-        self.homeDir        = os.path.expanduser("~/.sensomatic")
-        self.configFileName = self.homeDir + '/config.ini'
-        self.config         = ConfigParser.ConfigParser()
-        self.readConfig()
+        self._homeDir        = os.path.expanduser("~/.sensomatic")
+        self._configFileName = self._homeDir + '/config.ini'
+        self._config         = ConfigParser.ConfigParser()
+        self._readConfig()
 
+        self._mqclient = mqtt.Client("Persistor", clean_session=True)
+        self._redis    = redis.StrictRedis(host=self._config.get("REDIS", "ServerAddress"), port=self._config.get("REDIS", "ServerPort"), db=0)
+
+    def _on_connect(self, client, userdata, rc, msg):
+        print "Connected with result code %s" % rc
+        self._mqclient.subscribe("#")
+
+    def _on_message(self, client, userdata, msg):
+        print "Mq Received on channel %s -> %s" % (msg.topic, msg.payload)
+        self._redis.setex(msg.topic, 60 * 10, msg.payload)
+
+    def _on_disconnect(self, client, userdata, msg):
+        print "Disconnect"
 
     def run(self):
-        pass
-
-
-
-
-
+        self._mqclient.connect(self._config.get("MQTT","ServerAddress"), self._config.get("MQTT","ServerPort"), 60)
+        self._mqclient.on_connect = self._on_connect
+        self._mqclient.on_message = self._on_message
+        self._mqclient.on_disconnect = self._on_disconnect
+        self._mqclient.loop_forever()
 
 if __name__ == '__main__':
     print "Start"
 
     p = Persistor()
     p.start()
+
+    time.sleep(10)
+
+    print "End"
