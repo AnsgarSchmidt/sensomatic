2e8610a34fe1b62746039c7cdd585e368d77916f
Ansgar Schmidt <ansgar.schmidt@msasafety.com>
2015-06-21 22:48:33 +0200

    filling templates

--- server/Template.py
+++ server/Template.py
@@ -2,11 +2,13 @@ import datetime
 from jinja2 import Template
 from jinja2 import Environment
 from jinja2 import PackageLoader
+from InformationFetcher import  InformationFetcher
 
 class TemplateMatcher():
 
     def __init__(self):
-        self._env = Environment(loader=PackageLoader('Template', 'tts-templates'))
+        self._env                = Environment(loader=PackageLoader('Template', 'tts-templates'))
+        self._informationFetcher = InformationFetcher()
 
     def getHourlyTime(self):
         template = self._env.get_template('time-hourly.txt')
@@ -21,7 +23,12 @@ class TemplateMatcher():
         template = self._env.get_template('washingmachine-ready.txt')
         return template.render()
 
-
+    def getWakeupText(self, name):
+        template = self._env.get_template('wakeup.txt')
+        name = name
+        fraction, degrees, minutes, seconds = self._informationFetcher.getEarthRotationTime()
+        messages = self._informationFetcher.getNumEmailMessages()
+        return template.render(name=name, degrees=degrees, minutes=minutes, seconds=seconds, messages=messages)
 
 
 if __name__ == '__main__':
@@ -30,7 +37,7 @@ if __name__ == '__main__':
     print t.getHourlyTime()
     print t.getAcknowledgeStartWashingMachine()
     print t.getWashingMachineReady()
-
+    print t.getWakeupText('Ansi')
 
 
 
