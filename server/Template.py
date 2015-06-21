a246a04e89893b4a42132278e653d20e22dcae35
Ansgar Schmidt <ansgar.schmidt@msasafety.com>
2015-06-21 22:34:34 +0200

    Adding first examples

--- server/Template.py
+++ server/Template.py
@@ -1,19 +1,36 @@
+import datetime
 from jinja2 import Template
+from jinja2 import Environment
+from jinja2 import PackageLoader
 
 class TemplateMatcher():
 
-    TIME_HOURLY = "time-hourly.txt"
-
     def __init__(self):
-        pass
+        self._env = Environment(loader=PackageLoader('Template', 'tts-templates'))
+
+    def getHourlyTime(self):
+        template = self._env.get_template('time-hourly.txt')
+        hour = datetime.datetime.now().time().hour
+        return template.render(hour=hour)
+
+    def getAcknowledgeStartWashingMachine(self):
+        template = self._env.get_template('acknowledge-start-washing-machine.txt')
+        return template.render()
+
+    def getWashingMachineReady(self):
+        template = self._env.get_template('washingmachine-ready.txt')
+        return template.render()
+
 
 
 
 if __name__ == '__main__':
     print "Test"
     t = TemplateMatcher()
-    print t.HALLO
+    print t.getHourlyTime()
+    print t.getAcknowledgeStartWashingMachine()
+    print t.getWashingMachineReady()
 
-template = Template("Hello {{ name }}!")
-print template.render(name="Hugo humpf")
+
+
 
