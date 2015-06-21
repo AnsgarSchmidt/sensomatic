ac7dd6fd7ac7735df058b01a87783f06ce1a9f85
Ansgar Schmidt <ansgar.schmidt@msasafety.com>
2015-06-21 09:01:23 +0200

    Adding Persistor to main

--- server/main.py
+++ server/main.py
@@ -1,7 +1,13 @@
+import time
 
-
-
+from Persistor import Persistor
 
 if __name__ == '__main__':
 
     print "Starting"
+
+    print "Start Persistor"
+    persistor = Persistor()
+    persistor.setDaemon(False)
+    persistor.start()
+
