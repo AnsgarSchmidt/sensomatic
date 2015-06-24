9337f202fbfed48b7c1dec67ff6c1c9967a98c60
Ansgar Schmidt <ansgar.schmidt@msasafety.com>
2015-06-24 10:31:39 +0200

    Init

---
+++ server/Room.py
@@ -0,0 +1,11 @@
+
+class Room():
+
+    LIVING_ROOM = "Livingroom"
+    BATH_ROOM   = "Bathroom"
+    ANSI_ROOM   = "Ansiroom"
+    TIFFY_ROOM  = "Tiffyroom"
+    CORRIDOR    = "Corridor"
+
+    def __init__(self):
+        pass
