2f1a7dc69c0e51217c683727715b64371d2db61a
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2015-07-31 10:09:08 +0000

    First tests

--- knot/python/Plant.py
+++ knot/python/Plant.py
@@ -1,11 +1,30 @@
 import mraa
 import time
 
-led = mraa.Gpio(4)
-led.dir(mraa.DIR_OUT)
+pump = mraa.Pwm(3)
+pump.period_us(1000)
+pump.enable(True)
 
-for i in range(10):
-    led.write(1)
-    time.sleep(1)
-    led.write(0)
-    time.sleep(1)
+pump.write(0.0)
+
+#pump.write(1.0)
+#time.sleep(0.4)
+#pump.write(0.4)
+#time.sleep(1)
+#pump.write(0.5)
+#time.sleep(1)
+#pump.write(0.4)
+#time.sleep(1)  
+#pump.write(0.0)
+
+led = mraa.Pwm(5)
+led.period_us(700)
+led.enable(True)
+
+for i in range(100):
+	t = i / 100.0
+	print t
+	led.write(t)
+	time.sleep(1)
+
+led.write(0.0)
