8896077fe59080962fa2f6a5f3294c798fb3e9c0
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2015-08-02 12:42:03 +0200

    Infrastructure for plant check

--- server/main.py
+++ server/main.py
@@ -92,6 +92,9 @@ def bathShowerUpdate():
     else:
         print "No one showers"
 
+def plantCheck():
+    print "Plant check"
+
 if __name__ == '__main__':
 
     _readConfig()
@@ -116,6 +119,8 @@ if __name__ == '__main__':
     schedule.every(30).minutes.do(bathShowerUpdate)
 
     schedule.every().hour.at("00:00").do(hourAnnounce)
+
+    schedule.every().day.at("10:15").do(plantCheck)
 
     schedule.every().monday.at("07:00").do(wakeup,    "Ansi", Room.ANSI_ROOM)
     schedule.every().tuesday.at("07:00").do(wakeup,   "Ansi", Room.ANSI_ROOM)
