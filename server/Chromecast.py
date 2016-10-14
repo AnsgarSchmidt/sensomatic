aa6b1fc62dfe3435201f852c4f9701c4c0b52d60
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-10-14 09:56:30 +0200

    Init version

---
+++ server/Chromecast.py
@@ -0,0 +1,61 @@
+import pychromecast
+
+class Chromecast():
+
+    def __init__(self):
+        pass
+
+    def getNames(self):
+        return pychromecast.get_chromecasts_as_dict().keys()
+
+    def playMusicURL(self, castName, url):
+        cast = pychromecast.get_chromecast(friendly_name=castName)
+        cast.wait()
+        mc = cast.media_controller
+        mc.play_media(url, 'audio/mpeg')
+        mc.play()
+
+    def stop(self, castName):
+        cast = pychromecast.get_chromecast(friendly_name=castName)
+        cast.wait()
+        cast.quit_app()
+
+    def volume(self, castName, volume):
+        cast = pychromecast.get_chromecast(friendly_name=castName)
+        cast.wait()
+        cast.set_volume(volume)
+
+    def getVolume(self, castName):
+        cast = pychromecast.get_chromecast(friendly_name='Chromeansi')
+        cast.wait()
+        return cast.status.volume_level
+
+    def getDisplayName(self, castName):
+        cast = pychromecast.get_chromecast(friendly_name='Chromeansi')
+        cast.wait()
+        return cast.status.display_name
+
+    def test(self):
+        cast = pychromecast.get_chromecast(friendly_name='Chromeansi')
+        cast.wait()
+        print(cast.device)
+        print(cast.status)
+        mc = cast.media_controller
+        print(mc.status)
+        print (pychromecast.get_possible_app_ids())
+
+if __name__ == '__main__':
+    print "Test"
+    c = Chromecast()
+    #print c.getNames()
+    #c.playMusicURL('Chromeansi', 'http://inforadio.de/livemp3')
+    #time.sleep(1)
+    #c.volume('Chromeansi', 0.6)
+    #time.sleep(1)
+    #c.volume('Chromeansi', 0.1)
+    #time.sleep(1)
+    #c.stop('Chromeansi')
+    #print c.getVolume('Chromeansi')
+    #print c.getDisplayName('Chromeansi')
+    c.test()
+    #c.stop('Chromeansi')
