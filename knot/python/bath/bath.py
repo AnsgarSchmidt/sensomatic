46b938fe121a8116d0b55fb18a3b07be52e318db
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-10-30 18:53:18 +0100

    Updating to new Protocol

--- knot/python/bath/bath.py
+++ knot/python/bath/bath.py
@@ -15,7 +15,7 @@ class ReceiveThread(threading.Thread):
 
     def run(self):
         while True:
-            line = self._ser.readline()
+            line    = self._ser.readline()
             element = line.split(',')
             command = int(element[0])
             if (command == 0):
@@ -24,34 +24,32 @@ class ReceiveThread(threading.Thread):
             if (command == 1):
                 print "Error:%s" % line
             if (command == 2):
-                print "Set GRB"
+                print "Set R"
             if (command == 3):
                 print "Set R"
             if (command == 4):
-                print "Set G"
-            if (command == 5):
                 print "Set B"
-            if (command == 6):
+            if (command == 5):
                 print "GetTemp"
-            if (command == 7):
+            if (command == 6):
                 print "GetHumidity"
-            if (command == 8):
+            if (command == 7):
                 print "GetLight"
-            if (command == 9):
+            if (command == 10):
                 temp = float(element[1].split(";")[0])
                 #print "Temp:%f" % temp
                 self._mqclient.publish("bathroom/temperature", temp)
-            if (command == 10):
+            if (command == 11):
                 hum = float(element[1].split(";")[0])
                 #print "Humidity:%f"  % hum
                 self._mqclient.publish("bathroom/humidity", hum)
-            if (command == 11):
+            if (command == 12):
                 light = float(element[1].split(";")[0])
                 #print "Light:%d"  % light
                 self._mqclient.publish("bathroom/light", light)
-            if (command == 12):
+            if (command == 13):
                 button = int(element[1].split(";")[0])
-                #print "Button Pressed:%d" % button
+                print "Button Pressed:%d" % button
                 self._mqclient.publish("bathroom/button", button)
 
 class SendThread(threading.Thread):
@@ -67,7 +65,7 @@ class SendThread(threading.Thread):
             line = self._queue.get()
             self._ser.write(line + "\n")
             self._queue.task_done()
-            time.sleep(0.1)
+            time.sleep(0.5) # Give him time to react
 
 class TasksThread(threading.Thread):
     """Threaded measurement"""
@@ -79,9 +77,9 @@ class TasksThread(threading.Thread):
     def run(self):
         while True:
             time.sleep(5)
-            self._queue.put("6;") # Temperature
-            self._queue.put("7;") # Humidity
-            self._queue.put("8;") # Light
+            self._queue.put("5;") # Temperature
+            self._queue.put("6;") # Humidity
+            self._queue.put("7;") # Light
 
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
@@ -91,21 +89,18 @@ def on_message(client, userdata, msg):
     print "Mq Received on channel %s -> %s" % (msg.topic, msg.payload)
     parts   = msg.topic.split("/")
     channel = parts[2]
-    val     = int(msg.payload)
-    if channel == "r":
-        sendQueue.put("3,%d;" % val)
-    if channel == "g":
-        sendQueue.put("4,%d;" % val)
-    if channel == "b":
-        sendQueue.put("5,%d;" % val)
+    vals    = msg.payload.split(",")
+    sendQueue.put("2,%d;" % int(vals[0]))
+    sendQueue.put("3,%d;" % int(vals[1]))
+    sendQueue.put("4,%d;" % int(vals[2]))
 
 if __name__ == "__main__":
     print "Start"
 
-    ser = serial.Serial("/dev/ttyUSB0", 115200)
+    ser = serial.Serial("/dev/ttyAMA0", 9600)
 
     mqclient = mqtt.Client("bathroom", clean_session=True)
-    mqclient.connect("ansinas", 1883, 60)
+    mqclient.connect("cortex", 1883, 60)
     mqclient.on_connect = on_connect
     mqclient.on_message = on_message
 
@@ -120,10 +115,5 @@ if __name__ == "__main__":
     tasks = TasksThread(sendQueue)
     tasks.setDaemon(True)
     tasks.start()
-
-    for i in range(222):
-        sendQueue.put("3,%d;" % i)
-        sendQueue.put("4,%d;" % i)
-        sendQueue.put("5,%d;" % i)
 
     mqclient.loop_forever()
