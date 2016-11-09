3869440852697359b0eaecc6afb6d4833730fd9f
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-11-09 13:54:09 +0100

    Basic functionality

--- alexa/lambda_function.py
+++ alexa/lambda_function.py
@@ -1,4 +1,8 @@
 from __future__ import print_function
+import requests
+import socket
+import json
+import urllib2
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
@@ -30,8 +34,11 @@ def build_response(session_attributes, speechlet_response):
 def get_welcome_response():
     session_attributes = {}
     card_title         = "Welcome"
-    speech_output      = "Willkommen auf der U S S Horizon"
-    reprompt_text      = "Ja genau. Herzlich willkommen auf der USS Horizon"
+    speech_output      = """
+                         Willkommen auf der <say-as interpret-as=\"spell-out\">USS</say-as> Horizon.
+                         Ich kann zum Beispiel das Licht im wohnzimmer, kueche, esstisch, flur, ansiraum, tiffyraum, badezimmer
+                         an und ausschalten."""
+    reprompt_text      = "Ja genau. Herzlich willkommen auf der <say-as interpret-as=\"spell-out\">USS</say-as> Horizon"
     should_end_session = True
 
     return build_response(session_attributes, build_speechlet_response(
@@ -66,6 +73,15 @@ def handle_light(intent, session):
         state              = intent['slots']['state']['value']
         speech_output      = "Du hast den Raum " + room + " ausgesucht. Ich mache das Licht dort " + state
         should_end_session = True
+
+        passwd = ""
+        with open('passwd.txt', 'r') as myfile:
+            passwd = myfile.read().replace('\n', '')
+        command   = "light"
+        url       = "http://aws.23-5.eu:2342/api/v1.0/command"
+        data      = {"command" : command, "value" : state, "room": room, "pass": passwd}
+        print(requests.post(url, data).content)
+
     else:
         speech_output      = "Es wurde kein Raum ausgesucht."
         should_end_session = True
@@ -75,29 +91,24 @@ def handle_light(intent, session):
 
 def on_intent(intent_request, session):
 
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    print("on_intent requestId=" + intent_request['requestId'] + ", sessionId=" + session['sessionId'])
 
     intent      = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
-    # Dispatch to your skill's intent handlers
     if intent_name == "light":
         return handle_light(intent, session)
     else:
         raise ValueError("Invalid intent")
 
 def on_session_ended(session_ended_request, session):
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    print("on_session_ended requestId=" + session_ended_request['requestId'] + ", sessionId=" + session['sessionId'])
 
 def lambda_handler(event, context):
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
+    print("event.session.application.applicationId=" + event['session']['application']['applicationId'])
 
     if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
+        on_session_started({'requestId': event['request']['requestId']}, event['session'])
 
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'])
