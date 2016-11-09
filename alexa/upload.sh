f5fedb7a81f696e3739696a4112f54406326d465
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-11-09 13:53:27 +0100

    Adding passwd and requests

--- alexa/upload.sh
+++ alexa/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "AWS Lambda Update"
-zip -r ZipFiles/upload.zip lambda_function.py #requests/* requests-2.11.1.dist-info/*
+zip -9r ZipFiles/upload.zip lambda_function.py passwd.txt requests/* requests-2.11.1.dist-info/*
 aws lambda update-function-code --function-name arn:aws:lambda:us-east-1:834695880484:function:light --zip-file fileb://ZipFiles/upload.zip --region us-east-1
+rm ZipFiles/upload.zip
 echo "Done!!"
