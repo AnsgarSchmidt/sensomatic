c8cf6e33d013ad9d0f1ecc7bb0e671906a67efa7
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-01-31 16:56:24 +0100

    hunting the bug

--- knot/python/sysinfo.py
+++ knot/python/sysinfo.py
@@ -9,7 +9,8 @@ def on_message(client, userdata, msg):
     print "Mq Received on channel %s -> %s" % (msg.topic, msg.payload)
 
 def getLoadLevel():
-    res = os.popen('uptime | cut -c 40- | cut -f 2 -d ":" | cut -d "," -f 1').readline().replace("'C\n", "")
+    res = os.popen('uptime | cut -c 40- | cut -f 2 -d ":" | cut -d "," -f 1').readline().strip(\
+)
     return res
 
 # Return CPU temperature as a character string
