214b2442d5473302d18ce63af0c363818459a0a5
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-02-02 21:48:18 +0100

    init

---
+++ server/MongoDB.py
@@ -0,0 +1,8 @@
+import pymongo
+from   pymongo import MongoClient
+
+connection = MongoClient('localhost', 27017)
+db = connection.test
+names = db.names
+item = names.find_one()
+print item['name']
