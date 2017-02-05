c493e73ef3602673774c50d48847c37d5a1b5b3d
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-02-05 15:25:05 +0100

    mqtt connection

--- server/Telegram.py
+++ server/Telegram.py
@@ -1,10 +1,12 @@
 import os
 import time
+import Queue
 import telepot
+import threading
 import ConfigParser
 import paho.mqtt.client as mqtt
 
-class Telegram():
+class Telegram(threading.Thread):
 
     def _readConfig(self):
 
@@ -74,17 +76,30 @@ class Telegram():
                 self._config.write(f)
 
     def __init__(self):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
         self._homeDir                = os.path.expanduser("~/.sensomatic")
         self._configFileName         = self._homeDir + '/config.ini'
         self._config                 = ConfigParser.ConfigParser()
         self._readConfig()
         self._mqclient               = mqtt.Client("telegram", clean_session=True)
-        self._mqclient.connect("cortex", 1883, 60)
-        self._mqclient.loop_start()
         self._bot                    = telepot.Bot(self._config.get("TELEGRAM", "Key"))
+        self._workingQueue           = Queue.Queue()
+
+    def run(self):
+        self._mqclient.connect(self._config.get("MQTT","ServerAddress"), self._config.get("MQTT","ServerPort"), 60)
+        self._mqclient.on_connect    = self._on_connect
+        self._mqclient.on_message    = self._on_message
+        self._mqclient.on_disconnect = self._on_disconnect
+        self._mqclient.loop_start()
         self._bot.message_loop(self._handle)
         #print self._bot.getMe()
         self.sendMessage('Start Telegram Bot')
+        while True:
+            try:
+                self._process()
+            except Exception as e:
+                print e
 
     def _handle(self, msg):
         #print msg
@@ -92,10 +107,11 @@ class Telegram():
         uName  = msg['from']['username']
         uFName = msg['from']['first_name']
         text   = msg['text']
+
         if uID in self._members:
             self._bot.sendMessage(uID, 'Hey ' + uFName)
             if text == "help":
-                self._bot.sendMessage(uID, "I can deal with:\nhelp\nlight ansi\nlight kitchen")
+                self._bot.sendMessage(uID, "I can deal with:\nhelp\nlight ansi\nlight kitchen\nlight table\nlight living\nlight tiffy\nlight bath")
             if text == "light ansi":
                 self._bot.sendMessage(uID, "I switch the light in Ansi's room")
                 self._mqclient.publish("ansiroom/light/main", "TOGGLE")
@@ -105,13 +121,13 @@ class Telegram():
             if text == "light table":
                 self._bot.sendMessage(uID, "I switch the light at the table")
                 self._mqclient.publish("hackingroom/light/main", "TOGGLE")
-            if text == "light livingroom":
+            if text == "light living":
                 self._bot.sendMessage(uID, "I switch the light in the living room")
                 self._mqclient.publish("livingroom/light/main", "TOGGLE")
             if text == "light tiffy":
                 self._bot.sendMessage(uID, "I switch the light in Tiffy's room")
                 self._mqclient.publish("tiffyroom/light/main", "TOGGLE")
-            if text == "light bathroom":
+            if text == "light bath":
                 self._bot.sendMessage(uID, "I switch the light in the bath")
                 self._mqclient.publish("bathroom/light/main", "TOGGLE")
         else:
@@ -124,8 +140,24 @@ class Telegram():
         except Exception as e:
             print e
 
+    def _on_connect(self, client, userdata, rc, msg):
+        print "Connected MQTT Rulez with result code %s" % rc
+        self._mqclient.subscribe("telegram")
+
+    def _on_message(self, client, userdata, msg):
+        #print "Mq Received on channel %s -> %s" % (msg.topic, msg.payload)
+        self._workingQueue.put((msg.topic, msg.payload))
+
+    def _on_disconnect(self, client, userdata, msg):
+        print "Disconnect MQTTRulez"
+
+    def _process(self):
+        k, v = self._workingQueue.get()
+        keys = k.split("/")
+        if keys[0] == "telegram":
+            self.sendMessage(v)
+
 if __name__ == "__main__":
     t = Telegram()
-    time.sleep(3)
-    t.sendMessage("Hallo Welt")
+    t.start()
     time.sleep(42)
