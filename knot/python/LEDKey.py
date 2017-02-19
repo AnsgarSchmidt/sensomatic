bd6ff3ecfc2f30891c978a0cf695394b16809b59
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-02-19 11:23:02 +0100

    Catch error

--- knot/python/LEDKey.py
+++ knot/python/LEDKey.py
@@ -87,66 +87,68 @@ class LEDKey(threading.Thread):
     def run(self):
         while True:
 
-            # Display
-            if self.mode == 'timetemp':
-                now = datetime.datetime.now()
-                temp = 88
-                hum  = 88
-                if self._redis.exists("outside/temperature"):
-                    temp = float(self._redis.get("outside/feelslike"))
-                if self._redis.exists("outside/humidity"):
-                    hum = float(self._redis.get("outside/humidity"))
-                s = "%02d%02d%02d%02d" % (now.hour, now.minute, temp, hum)
-                self.tm.set_text(s)
+            try:
+                # Display
+                if self.mode == 'timetemp':
+                    now = datetime.datetime.now()
+                    temp = 88
+                    hum  = 00
+                    if self._redis.exists("outside/temperature"):
+                        temp = float(self._redis.get("outside/feelslike"))
+                    if self._redis.exists("outside/humidity"):
+                        hum = float(self._redis.get("outside/humidity"))
+                    s = "%02d%02d%02d%02d" % (now.hour, now.minute, temp, hum)
+                    self.tm.set_text(s)
 
-            # LED 0 -> Heater Tank
-            if self._redis.exists("livingroom/tank/heater"):
-                heater = float(self._redis.get("livingroom/tank/heater"))
-                if heater == 1.0:
-                    self.tm.set_led(0, 1)
-                else:
-                    self.tm.set_led(0, 0)
+                # LED 0 -> Heater Tank
+                if self._redis.exists("livingroom/tank/heater"):
+                    heater = float(self._redis.get("livingroom/tank/heater"))
+                    if heater == 1.0:
+                        self.tm.set_led(0, 1)
+                    else:
+                        self.tm.set_led(0, 0)
 
-            # LED 1 -> Heater Waterlevel
-            if self._redis.exists("livingroom/tank/waterlevel"):
-                heater = float(self._redis.get("livingroom/tank/waterlevel"))
-                if heater == 1.0:
-                    self.tm.set_led(1, 1)
-                else:
-                    self.tm.set_led(1, 0)
+                # LED 1 -> Heater Waterlevel
+                if self._redis.exists("livingroom/tank/waterlevel"):
+                    heater = float(self._redis.get("livingroom/tank/waterlevel"))
+                    if heater == 1.0:
+                        self.tm.set_led(1, 1)
+                    else:
+                        self.tm.set_led(1, 0)
 
-            # LED 2 -> Heater Whitelight
-            if self._redis.exists("livingroom/tank/whitelight"):
-                whitelight = float(self._redis.get("livingroom/tank/whitelight"))
-                if whitelight > 0.0:
-                    self.tm.set_led(2, 1)
-                else:
-                    self.tm.set_led(2, 0)
+                # LED 2 -> Heater Whitelight
+                if self._redis.exists("livingroom/tank/whitelight"):
+                    whitelight = float(self._redis.get("livingroom/tank/whitelight"))
+                    if whitelight > 0.0:
+                        self.tm.set_led(2, 1)
+                    else:
+                        self.tm.set_led(2, 0)
 
-            # LED 3 -> Heater Whitelight
-            if self._redis.exists("livingroom/tank/bluelight"):
-                bluelight = float(self._redis.get("livingroom/tank/bluelight"))
-                if bluelight > 0.0:
-                    self.tm.set_led(3, 1)
-                else:
-                    self.tm.set_led(3, 0)
+                # LED 3 -> Heater Whitelight
+                if self._redis.exists("livingroom/tank/bluelight"):
+                    bluelight = float(self._redis.get("livingroom/tank/bluelight"))
+                    if bluelight > 0.0:
+                        self.tm.set_led(3, 1)
+                    else:
+                        self.tm.set_led(3, 0)
 
-            # LED 4 -> Ansi here
-            if self._redis.exists("ansi/wlanPresents"):
-                ansi = int(self._redis.get("ansi/wlanPresents"))
-                if ansi == 1:
-                    self.tm.set_led(4, 1)
-                else:
-                    self.tm.set_led(4, 0)
+                # LED 4 -> Ansi here
+                if self._redis.exists("ansi/wlanPresents"):
+                    ansi = int(self._redis.get("ansi/wlanPresents"))
+                    if ansi == 1:
+                        self.tm.set_led(4, 1)
+                    else:
+                        self.tm.set_led(4, 0)
 
-            # LED 5 -> Tiffy here
-            if self._redis.exists("tiffy/wlanPresents"):
-                tiffy = int(self._redis.get("tiffy/wlanPresents"))
-                if tiffy == 1:
-                    self.tm.set_led(5, 1)
-                else:
-                    self.tm.set_led(5, 0)
-
+                # LED 5 -> Tiffy here
+                if self._redis.exists("tiffy/wlanPresents"):
+                    tiffy = int(self._redis.get("tiffy/wlanPresents"))
+                    if tiffy == 1:
+                        self.tm.set_led(5, 1)
+                    else:
+                        self.tm.set_led(5, 0)
+            except Exception as e:
+                print e
             time.sleep(1)
 
 if __name__ == "__main__":
