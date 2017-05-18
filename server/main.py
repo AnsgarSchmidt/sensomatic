b49b93639687f7940b9cb64e748d0459a02fd879
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-05-18 23:50:54 +0200

    Replacing tts

--- server/main.py
+++ server/main.py
@@ -1,17 +1,17 @@
 import os
+import sys
 import time
 import redis
 import schedule
 import ConfigParser
+import paho.mqtt.client   as     mqtt
 from   CloudantDB         import CloudantDB
 from   InformationFetcher import InformationFetcher
-from   Tts                import Tts
 from   Template           import TemplateMatcher
 from   Persistor          import Persistor
 from   Room               import Room
 from   MqttRulez          import MqttRulez
 from   Pinger             import Pinger
-from   InitialState       import InitialState
 from   Climate            import Climate
 from   RoomController     import RoomController
 from   AlarmClock         import AlarmClock
@@ -19,12 +19,10 @@ from   HS100              import HS100
 from   Mpd                import Mpd
 from   TwitterPusher      import TwitterPusher
 from   Tank               import Tank
-from   Carbon             import Carbon
 from   Telegram           import Telegram
 from   SmarterCoffee      import SmartCoffee
 
 temp = TemplateMatcher()
-tts  = Tts()
 info = InformationFetcher()
 
 homeDir        = os.path.expanduser("~/.sensomatic")
@@ -65,15 +63,31 @@ def _readConfig():
         update = True
         config.set("REDIS", "ServerPort", "6379")
 
+    if not self._config.has_section('MQTT'):
+        print "Adding MQTT part"
+        update = True
+        config.add_section("MQTT")
+
+    if not self._config.has_option("MQTT", "ServerAddress"):
+        print "No Server Address"
+        update = True
+        config.set("MQTT", "ServerAddress", "<ServerAddress>")
+
+    if not self._config.has_option("MQTT", "ServerPort"):
+        print "No Server Port"
+        update = True
+        config.set("MQTT", "ServerPort", "1883")
+
     if update:
         with open(configFileName, 'w') as f:
             config.write(f)
+            sys.exit(1)
 
 def hourAnnounce():
     print "Announce hour"
     for room in Room.ANNOUNCE_ROOMS:
         if info.isSomeoneInTheRoom(room):
-            tts.createWavFile(temp.getHourlyTime(), room)
+            _mqclient.publish("%s/ttsout" % room, temp.getHourlyTime())
 
 def checkWaschingMachine():
     print "Check washing machine"
@@ -82,19 +96,19 @@ def checkWaschingMachine():
         timestamp = float(_redis.get("WashingmachineReady"))
         for room in Room.ANNOUNCE_ROOMS:
             if info.isSomeoneInTheRoom(room):
-                tts.createWavFile(temp.getWashingMachineReady(timestamp), room)
+                _mqclient.publish("%s/ttsout" % room, temp.getWashingMachineReady(timestamp))
     else:
         print "Wasching machine inactive"
 
 def goSleep():
     print "Go to sleep"
-    tts.createWavFile(temp.getTimeToGoToBed(), Room.LIVING_ROOM)
+    _mqclient.publish("livingroom/ttsout", temp.getTimeToGoToBed())
 
 def checkBath():
     print "Checking bath"
     if _redis.exists("PlayRadioInBath") and not info.isSomeoneInTheRoom(Room.BATH_ROOM):
         Mpd().getServerbyName("Bath").stop()
-        #self._mqclient.publish("bathroom/light/rgb", "0,0,0")
+        _mqclient.publish("bathroom/light/rgb", "0,0,0")
         _redis.delete("PlayRadioInBath")
 
 def checkCo2(room):
@@ -103,7 +117,7 @@ def checkCo2(room):
         if info.isSomeoneInTheRoom(room):
             if info.getRoomCo2Level(room) is not None and info.getRoomCo2Level(room) > 2300:
                 print "CO2 to high:" + str(info.getRoomCo2Level(room))
-                tts.createWavFile(temp.getCo2ToHigh(room), room)
+                _mqclient.publish("%s/ttsout" % room, temp.getCo2ToHigh(room))
 
 def radiationCheck():
     print "Radiation check"
@@ -111,11 +125,11 @@ def radiationCheck():
     here = info.getRadiationForOneStation()
     if here > 0.15:
         for room in Room.ANNOUNCE_ROOMS:
-            tts.createWavFile(temp.getRadiationToHigh(here), room)
+            _mqclient.publish("%s/ttsout" % room, temp.getRadiationToHigh(here))
     if here > avr:
         for room in Room.ANNOUNCE_ROOMS:
             if info.isSomeoneInTheRoom(room):
-                tts.createWavFile(temp.getRadiationHigherThenAverage(here, avr), room)
+                _mqclient.publish("%s/ttsout" % room, temp.getRadiationHigherThenAverage(here, avr))
 
 def particulateMatterCheck():
     print "ParticularMatterCheck"
@@ -123,19 +137,37 @@ def particulateMatterCheck():
     if p1 > 23.0 or p2 > 23.0:
         for room in Room.ANNOUNCE_ROOMS:
             if info.isSomeoneInTheRoom(room):
-                tts.createWavFile(temp.getParticulateMatterHigherThenAverage(p1, p2), room)
+                _mqclient.publish("%s/ttsout" % room, temp.getParticulateMatterHigherThenAverage(p1, p2))
 
 def bathShowerUpdate():
     print "Checking Bath and Shower conditions"
     if info.getBathOrShower() is not None:
-        tts.createWavFile(temp.getBathShowerUpdate(), Room.BATH_ROOM)
+        _mqclient.publish("bathroom/ttsout", temp.getBathShowerUpdate())
     else:
         print "No one showers"
+
+def _on_connect(client, userdata, rc, msg):
+    print "Connected MQTT Main with result code %s" % rc
+    #self._mqclient.subscribe("#")
+
+def _on_message(client, userdata, msg):
+    #print "Mq Received on channel %s -> %s" % (msg.topic, msg.payload)
+    #self._workingQueue.put((msg.topic, msg.payload))
+
+def _on_disconnect(client, userdata, msg):
+    print "Disconnect MQTTRulez"
+
 
 if __name__ == '__main__':
 
     _readConfig()
-    _redis = redis.StrictRedis(host=config.get("REDIS", "ServerAddress"), port=config.get("REDIS", "ServerPort"), db=0)
+    _redis                  = redis.StrictRedis(host=config.get("REDIS", "ServerAddress"), port=config.get("REDIS", "ServerPort"), db=0)
+    _mqclient               = mqtt.Client("Main", clean_session=True)
+    _mqclient.connect(config.get("MQTT", "ServerAddress"), config.get("MQTT", "ServerPort"), 60)
+    _mqclient.on_connect    = _on_connect
+    _mqclient.on_message    = _on_message
+    _mqclient.on_disconnect = _on_disconnect
+    _mqclient.loop_start()
     _wait_time = 5
 
     print "Start Persistor"
