b0860743b76830cbda30529b6c88775586040b44
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-07-22 22:07:46 +0200

    Basic Version

---
+++ server/Influx.py
@@ -0,0 +1,137 @@
+import os
+import sys
+import time
+import Queue
+import threading
+import ConfigParser
+import paho.mqtt.client as     mqtt
+from   influxdb         import InfluxDBClient
+
+
+class Influx(threading.Thread):
+
+    def _readConfig(self):
+        update = False
+
+        if not os.path.isdir(self._homeDir):
+            print "Creating homeDir"
+            os.makedirs(self._homeDir)
+
+        if os.path.isfile(self._configFileName):
+            self._config.read(self._configFileName)
+        else:
+            print "Config file not found"
+            update = True
+
+        if not self._config.has_section('MQTT'):
+            print "Adding MQTT part"
+            update = True
+            self._config.add_section("MQTT")
+
+        if not self._config.has_option("MQTT", "ServerAddress"):
+            print "No Server Address"
+            update = True
+            self._config.set("MQTT", "ServerAddress", "<ServerAddress>")
+
+        if not self._config.has_option("MQTT", "ServerPort"):
+            print "No Server Port"
+            update = True
+            self._config.set("MQTT", "ServerPort", "1883")
+
+        if not self._config.has_section('INFLUX'):
+            print "Adding INFLUX part"
+            update = True
+            self._config.add_section("INFLUX")
+
+        if not self._config.has_option("INFLUX", "ServerAddress"):
+            print "No Server Address"
+            update = True
+            self._config.set("INFLUX", "ServerAddress", "<ServerAddress>")
+
+        if not self._config.has_option("INFLUX", "ServerPort"):
+            print "No Server Port"
+            update = True
+            self._config.set("INFLUX", "ServerPort", "8086")
+
+        if update:
+            with open(self._configFileName, 'w') as f:
+                self._config.write(f)
+                sys.exit(0)
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+
+        self._homeDir        = os.path.expanduser("~/.sensomatic")
+        self._configFileName = self._homeDir + '/config.ini'
+        self._config         = ConfigParser.ConfigParser()
+        self._readConfig()
+
+        self._workingQueue = Queue.Queue()
+
+        self._mqclient = mqtt.Client("Influx", clean_session=True)
+        self._mqclient.on_connect    = self._on_connect
+        self._mqclient.on_message    = self._on_message
+        self._mqclient.on_disconnect = self._on_disconnect
+        self._mqclient.connect(self._config.get("MQTT", "ServerAddress"), self._config.get("MQTT", "ServerPort"), 60)
+
+        self._influx = InfluxDBClient('hal', 8086, 'root', 'root', 'example')
+        self._check_db()
+
+    def _check_db(self):
+
+        exists = False
+
+        for e in self._influx.get_list_database():
+            if e['name'] == "horizon":
+                exists = True
+
+        if exists:
+            print "Influx DB does exists"
+            # client.drop_database('example')
+        else:
+            print "Influx DB does not exists, create one"
+            self._influx.create_database("horizon")
+
+    def _on_connect(self, client, userdata, rc, msg):
+        print "Connected Influx with result code %s" % rc
+        self._mqclient.subscribe("#")
+
+    def _on_message(self, client, userdata, msg):
+        pass
+        #print "Mq Received on channel %s -> %s" % (msg.topic, msg.payload)
+        self._workingQueue.put((msg.topic, msg.payload))
+
+    def _on_disconnect(self, client, userdata, msg):
+        print "Disconnect Influx"
+
+    def _process(self):
+        k, v = self._workingQueue.get()
+        keys = k.split("/")
+
+        #print keys
+        #print v
+
+    def run(self):
+        self._mqclient.connect(self._config.get("MQTT", "ServerAddress"), self._config.get("MQTT", "ServerPort"), 60)
+        self._mqclient.on_connect    = self._on_connect
+        self._mqclient.on_message    = self._on_message
+        self._mqclient.on_disconnect = self._on_disconnect
+        self._mqclient.loop_start()
+        while True:
+            try:
+                self._process()
+            except Exception as e:
+                self._logger.error("Error in processing")
+                self._logger.error(e)
+
+
+if __name__ == '__main__':
+    print "Start"
+
+    i = Influx()
+    i.start()
+
+    time.sleep(10)
+
+    print "End"
